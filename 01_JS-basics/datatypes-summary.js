a1d7580ff550ecca4655975e28a782e590d868a0
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-05 07:44:44 +0000

    stack and heap memory

--- 01_JS-basics/datatypes-summary.js
+++ 01_JS-basics/datatypes-summary.js
@@ -6,4 +6,8 @@
 
 // Arrays, Objects, Functions
 
+// Stack memory use in Primitive and give us a copy  
+// Heap memory use in Reference data type and give us a original value refrence
+
+
 // https://262.ecma-international.org/5.1/#sec-11.4.3
