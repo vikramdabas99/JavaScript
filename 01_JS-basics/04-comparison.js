7a60d7b39f248a91e43cc4db936a816188f08591
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-05 07:28:35 +0000

    comparison and datatypes summary

---
+++ 01_JS-basics/04-comparison.js
@@ -0,0 +1,9 @@
+// console.log(2>2);
+// console.log(2<2);
+// console.log(2>=2);
+// console.log(2<=2);
+// console.log(2==2);
+// console.log(2!=2);
+
+// === strict check
+console.log('2' === 2);
---
+++ 01_JS-basics/datatypes-summary.js
@@ -0,0 +1,9 @@
+// Primitive Data types
+
+// 7 types :- String, Number, Boolean, null, undefined, Symbol, BigInt
+
+// Refernce (Non Primitive) Data types
+
+// Arrays, Objects, Functions
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
