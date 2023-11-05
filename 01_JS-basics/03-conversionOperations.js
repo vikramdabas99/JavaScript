f3d0bba48a4c99c46b2c925075329a5c6b690caf
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-05 05:50:14 +0000

    conversion operator

---
+++ 01_JS-basics/03-conversionOperations.js
@@ -0,0 +1,39 @@
+/* ---------- Conversion ---------- */
+
+let score = '33'
+
+// console.log(Number(score));
+
+/*
+'33' = 33
+'12acs' = NaN(not a number)
+true = 1
+false = 0
+null = 0
+undefined = NaN
+*/
+
+let isLoggedIn = 1
+
+/*
+1 = true
+0 = false
+"" = false
+'vikram' = true
+*/
+
+// console.log(Boolean(isLoggedIn));
+
+let someNumber = 23
+console.log(String(someNumber));
+
+/* ---------- Operations ---------- */
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/4);
+// console.log(2%4);
+
+console.log((2 + 3) * 4 % 3);
