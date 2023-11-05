ae83cc7806061599697f6632c6115db561634452
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-05 03:30:21 +0000

    datatypes commit

---
+++ 01_JS-basics/02-datatypes.js
@@ -0,0 +1,19 @@
+'use strict'; // treat all JS code as newer version of JS
+
+// alert('hello') // we are using nodejs not browser
+
+// primitive datatypes
+/*
+number = 2 power of 53
+bigint = for big numbers
+string = "" for words
+boolean = true or false 
+null = stand alone value/ empty 
+undefined =  not assign a value 
+symbol = find the uniqueness
+*/
+
+/*
+object = {key: value} pair
+array = []
+*/
