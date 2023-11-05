25d3554945fcd9c1ce4553ae8bd28bbe1d4f9153
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-05 03:10:06 +0000

    javascript variables

---
+++ 01_JS-basics/01-variables.js
@@ -0,0 +1,13 @@
+const accountId = 'vikram11'
+let accountEmail = 'vikram@gm.com'
+var accountPassword = '123445'
+accountCity = 'haryana'
+let accountState; // default state is undefined
+
+// accountId = 'monu11' // cannot change const variable, it is not allowed
+
+accountEmail = 'vd@vd.com'
+accountPassword = '213141'
+accountCity = 'delhi'
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
