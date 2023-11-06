3be9f1ff36dfc44003a8b40efc0de7cfd246e4ee
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-06 15:38:36 +0000

    nums and math

--- 01_JS-basics/06-nums-and-math.js
+++ 01_JS-basics/06-nums-and-math.js
@@ -1,5 +1,27 @@
+// ************ Numbers ***********
 const score = 400;
-console.log(score);
+// console.log(score);
 
 const balance = new Number(100)
-console.log(balance);
+// console.log(balance);
+// console.log(balance.toFixed(2));
+
+const number = 100.5228
+// console.log(number.toPrecision(3));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ************ Math ***********
+// console.log(Math.abs(-4)); // make -ve value +ve
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.6));
+// console.log(Math.random());
+// console.log((Math.random() * 10) + 1);
+// console.log(Math.floor((Math.random() * 10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min));
