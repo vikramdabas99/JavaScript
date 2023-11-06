372342ba4e79448663aeebe9e56c5ce66ea1cb18
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-06 03:29:26 +0000

    strings methods

--- 01_JS-basics/05-strings.js
+++ 01_JS-basics/05-strings.js
@@ -3,4 +3,24 @@
 const name = 'vikram'
 const age = 28
 
-console.log(`Hey my name is ${name} and i'm ${age} years old. `);
+// console.log(`Hey my name is ${name} and i'm ${age} years old. `);
+
+const userName = 'vikramdabas'
+const str = 'Hello my name is monu.'
+const greet = userName.concat(' ',str)
+
+const spaces = '      hey monu!    ' // remove the spaces from starting and ending bt not in middle of text
+
+console.log(userName);
+console.log(userName.length);
+console.log(userName.charAt(3));
+console.log(userName.indexOf('d'));
+console.log(userName.substring(2, 5)); // starting index is include bt not last 
+console.log(str.split(' '));
+console.log(greet);
+console.log(spaces.trim());
+console.log(userName.toUpperCase());
+console.log(userName.toLowerCase());
+console.log(userName.replace('vikram', 'monu'));
+console.log(str.includes('my'));
+console.log(userName.slice(2, 5));
