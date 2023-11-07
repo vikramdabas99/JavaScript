f1b927c44a2422bdd0ca97b1a30975b070fab07f
vikramdabas99 <57287175+vikramdabas99@users.noreply.github.com>
2023-11-07 02:21:12 +0000

    array

---
+++ 02_basics/01-arrays.js
@@ -0,0 +1,34 @@
+// array :- declaring multipule values in a single variable
+
+const myArr = [1,2,3,4,5]
+const heros = ['shaktiman', 'nagraaj']
+
+const newArr = new Array(1,2,3,4,5)
+// console.log(myArr[3]); // access by index
+
+// Array methods
+// myArr.push(7) // Add a value in the last index
+// myArr.pop() // Remove the last index value 
+
+// myArr.unshift(0) // Add a value in starting index
+// myArr.shift() // remove index first value
+
+// console.log(myArr.includes(2));
+// console.log(myArr.indexOf(2));
+
+// const newArray = myArr.join() // convert into a string
+// console.log(newArray);
+
+// console.log(myArr);
+
+// slice, splice
+// console.log('A ', myArr);
+
+// const myn1 = myArr.slice(1, 3) // not incllude last value
+
+// console.log(myn1);
+// console.log('B ', myArr);
+
+// const myn2 = myArr.splice(1, 3) // include last value also and manipulate the original array and return the remaning values
+// console.log('C ', myArr);
+// console.log(myn2);
